803ff5b61a88a5ae9520dbe4424c2ce036c63622
Lang-325 <lyn7251@stu.ouc.edu.cn>
2025-09-09 11:52:38 +0800

    Add files via upload

---
+++ store.h
@@ -0,0 +1,109 @@
+#pragma once
+#ifndef STORE_H
+#define STORE_H
+
+#include <memory>
+#include "player.h"
+#include <string>
+#include <algorithm>
+#include "storage.h"
+
+class Store {
+public:
+    Store() = default;
+    ~Store() = default;
+
+    void ShowMenu(CPlayer& player, Storage& inventory) {
+        bool inStore = true;
+        while (inStore) {
+            system("cls");
+            std::cout << "===== 商店 =====" << std::endl;
+            std::cout << "你的金币: " << player.GetGold() << std::endl;
+            std::cout << "1. 药水 (50金币)" << std::endl;
+            std::cout << "2. 枪 (100金币)" << std::endl;
+            std::cout << "3. 护甲 (80金币)" << std::endl;
+            std::cout << "4. 刀 (70金币)" << std::endl;
+            std::cout << "5. 离开" << std::endl;
+            std::cout << "请选择: ";
+
+            std::string command;
+            std::cin >> command;
+            std::transform(command.begin(), command.end(), command.begin(), [](unsigned char c) { return std::tolower(c); });
+
+            if (command == "药水" || command == "yaoshui") {
+                BuyPotion(player, inventory);
+            }
+            else if (command == "枪" || command == "qiang") {
+                BuyGun(player, inventory);
+            }
+            else if (command == "盔甲" || command == "kuijia") {
+                BuyArmor(player, inventory);
+            }
+            else if (command == "刀" || command == "dao") {
+                BuyKnife(player, inventory);
+            }
+            else if (command == "离开" || command == "likai") {
+                inStore = false;
+                std::cout << "感谢光临！" << std::endl;
+            }
+            else {
+                std::cout << "无效命令，请重新输入！" << std::endl;
+            }
+
+            if (inStore) system("pause");
+        }
+    }
+
+private:
+    void BuyPotion(CPlayer& player, Storage& inventory) {
+        if (player.GetGold() >= 1) {
+            player.AddGold(-1);
+            auto potion = std::make_shared<Item>("强效药水", "恢复50生命值", ItemType::POTION);
+            potion->setAttribute(AttributeType::HP, 50);
+            inventory.storeItem(potion);
+            std::cout << "获得强效药水！" << std::endl;
+        }
+        else {
+            std::cout << "金币不足！" << std::endl;
+        }
+    }
+    void BuyGun(CPlayer& player, Storage& inventory) {
+        if (player.GetGold() >= 1) {
+            player.AddGold(-1);
+            auto gun = std::make_shared<Item>("手枪", "攻击+10", ItemType::WEAPON);
+            gun->setAttribute(AttributeType::ATTACK, 10);
+            inventory.storeItem(gun);
+            std::cout << "获得手枪！" << std::endl;
+        }
+        else {
+            std::cout << "金币不足！" << std::endl;
+        }
+    }
+    void BuyArmor(CPlayer& player, Storage& inventory) {
+        if (player.GetGold() >= 1) {
+            player.AddGold(-1);
+            auto armor = std::make_shared<Item>("护甲", "防御+5", ItemType::ARMOR);
+            armor->setAttribute(AttributeType::DEFENSE, 5);
+            inventory.storeItem(armor);
+            std::cout << "获得护甲！" << std::endl;
+        }
+        else {
+            std::cout << "金币不足！" << std::endl;
+        }
+    }
+    void BuyKnife(CPlayer& player, Storage& inventory) {
+        if (player.GetGold() >= 1) {
+            player.AddGold(-1);
+            auto knife = std::make_shared<Item>("匕首", "攻击+3 敏捷+5", ItemType::WEAPON);
+            knife->setAttribute(AttributeType::ATTACK, 3);
+            knife->setAttribute(AttributeType::AGILITY, 5);
+            inventory.storeItem(knife);
+            std::cout << "获得匕首！" << std::endl;
+        }
+        else {
+            std::cout << "金币不足！" << std::endl;
+        }
+    }
+};
+
+#endif
