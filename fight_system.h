0afb2cc64f06153ed35a4044cc18f54b0b815e94
fkyoda <mazheyuan0810@sina.com>
2025-09-09 10:08:05 +0800

    Add files via upload

---
+++ fight_system.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef FIGHT_SYSTEM_H
+#define FIGHT_SYSTEM_H
+
+#include <memory>
+#include "player.h"
+#include "enemy.h"
+#include "global.h"
+#include "storage.h"
+
+class CFightSystem {
+private:
+    std::unique_ptr<CPlayer> m_player;
+    std::unique_ptr<CEnemy> m_enemy;
+    Storage& m_inventory;
+    bool m_isPlayerTurn;
+
+public:
+    CFightSystem(std::unique_ptr<CPlayer> player, std::unique_ptr<CEnemy> enemy, Storage& inventory);
+    ~CFightSystem(){}
+
+    std::unique_ptr<CPlayer> releasePlayer() { return std::move(m_player); }
+    void StartFight();
+    void ShowFightPanel() const;
+    void DecideFirstAttacker();
+    bool IsHit(float defenderAgility);
+    float CalculateDamage(float attackerAtk, float defenderDef, float multiplier = 1.0f);
+    bool UsePotionInBattle();
+};
+
+#endif
